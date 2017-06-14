6ac7f74ed984a4353a323a9f631b3da6543093f8
Van Roye <steven.van-roye@sogeti.com>
2017-06-14 22:26:28 +0200

    Use of separate variable for storing data

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-var ViewModel = function() {
-	this.name = ko.observable('Tommy');
+var Cat= function() {
+	this.name = ko.observable('Jimmy');
 	this.clickCount = ko.observable(0);
 	this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
-
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
-	}
+	this.nicknames = ko.observableArray([
+		{name: "St. Jimmy"}, 
+		{name: "Jesus of Suburbia"}, 
+		{name: "Whatshername"}
+		]);
 
 	this.level = ko.computed(function(){
 		if(this.clickCount() < 10){
@@ -16,6 +17,15 @@ var ViewModel = function() {
 			return "Adult";
 		}
 	},this);
+}
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable(new Cat())
+
+	this.incrementCounter = function(){
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	}
 }
 
 ko.applyBindings(new ViewModel());
